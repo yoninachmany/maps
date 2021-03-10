648211e64b819211c5c4ceaa66a05475da53e5f6
Yoni Nachmany <yoni.nachmany@mapbox.com>
2021-03-10 12:58:22 -0500

    Mapbox imagery updates in 2020

---
+++ 2020/config.js
@@ -0,0 +1,267 @@
+var config = {
+    style: 'mapbox://styles/mapboxsatellite/ckignln2h11dy1aogi9so6szo',
+    accessToken: 'pk.eyJ1IjoibWFwYm94c2F0ZWxsaXRlIiwiYSI6ImNqZWZ0MHg0djFqZWoyeG9kN3ZiMmkyd3cifQ.y2HNjGo7FcKQ7psI_BfGqQ',
+    showMarkers: false,
+    markerColor: '#3FB1CE',
+    theme: 'dark',
+    use3dTerrain: true,
+    title: '14 Updates to Mapbox Satellite in 2020',
+    subtitle: 'Open Aerial Imagery (and One Big Purchase!)',
+    byline: "By Mapbox's Imagery team",
+    footer: 'To another year of open data updates!',
+    chapters: [
+        {
+            id: 'lyon',
+            alignment: 'left',
+            hidden: false,
+            title: '1. Lyon, France',
+            description: '<ul><li><b>Recency</b>: 2018</li><li><b>Resolution</b>: 8 cm</li></ul>',
+            location: {
+                center: [4.81740, 45.73211],
+                zoom: 17.00,
+                pitch: 0,
+                bearing: -78.40
+            },
+            mapAnimation: 'jumpTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'berlin',
+            alignment: 'right',
+            hidden: false,
+            title: '2. Berlin, Germany',
+            description: '<ul><li><b>Recency</b>: 2019</li><li><b>Resolution</b>: 15 cm</li></ul>',
+            location: {
+                center: [13.378126, 52.517749],
+                zoom: 16.57,
+                pitch: 0,
+                bearing: -96
+            },
+            mapAnimation: 'jumpTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'gothenburg',
+            alignment: 'left',
+            hidden: false,
+            title: '3. Gothenburg, Sweden',
+            description: '<ul><li><b>Recency</b>: 2019</li><li><b>Resolution</b>: 25 cm</li></ul>',
+            location: {
+                center: [11.85772, 57.69222],
+                zoom: 15.01,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'jumpTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'zurich',
+            alignment: 'right',
+            hidden: false,
+            title: '4. Zurich, Switzerland',
+            description: '<ul><li><b>Recency</b>: 2018</li><li><b>Resolution</b>: 10 cm</li></ul>',
+            location: {
+                center: [8.52453, 47.35672],
+                zoom: 14.9,
+                pitch: 76,
+                bearing: -93
+            },
+            mapAnimation: 'jumpTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'bayonne',
+            alignment: 'left',
+            hidden: false,
+            title: '5. Bayonne, France',
+            description: '<ul><li><b>Recency</b>: 2016</li><li><b>Resolution</b>: 6 cm</li></ul>',
+            location: {
+                center: [-1.47432, 43.49100],
+                zoom: 19,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'jumpTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'nancy',
+            alignment: 'right',
+            hidden: false,
+            title: '6. Nancy, France',
+            description: '<ul><li><b>Recency</b>: 2016</li><li><b>Resolution</b>: 5 cm</li></ul>',
+            location: {
+                center: [6.18013, 48.69565],
+                zoom: 19,
+                pitch: 0,
+                bearing: 68
+            },
+            mapAnimation: 'jumpTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'rennes',
+            alignment: 'left',
+            hidden: false,
+            title: '7. Rennes, France',
+            description: '<ul><li><b>Recency</b>: 2017</li><li><b>Resolution</b>: 10 cm</li></ul>',
+            location: {
+                center: [-1.66772, 48.11483],
+                zoom: 18.51,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'jumpTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'ign-france',
+            alignment: 'right',
+            hidden: false,
+            title: '8. IGN - France',
+            description: '<ul><li><b>Recency</b>: 2016-2020</li><li><b>Resolution</b>: 15-50 cm</li></ul>',
+            location: {
+                center: [5.83031, 44.77512],
+                zoom: 15,
+                pitch: 60,
+                bearing: 0
+            },
+            mapAnimation: 'jumpTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'mulhouse',
+            alignment: 'left',
+            hidden: false,
+            title: '9. Mulhouse, France',
+            description: '<ul><li><b>Recency</b>: 2018</li><li><b>Resolution</b>: 8 cm</li></ul>',
+            location: {
+				center: [7.34049, 47.74407],
+				zoom: 18.23,
+				pitch: 0.00,
+				bearing: 0.00
+            },
+            mapAnimation: 'jumpTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'nrw',
+            alignment: 'right',
+            hidden: false,
+            title: '10. North Rhine-Westphalia, Germany',
+            description: '<ul><li><b>Recency</b>: 2017-2019</li><li><b>Resolution</b>: 10 cm</li></ul>',
+            location: {
+				center: [6.96028, 50.94321],
+				zoom: 15.67,
+				pitch: 0.00,
+				bearing: 0.00
+            },
+            mapAnimation: 'jumpTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'melbourne',
+            alignment: 'left',
+            hidden: false,
+            title: '11. Melbourne, Australia',
+            description: '<ul><li><b>Recency</b>: 2019</li><li><b>Resolution</b>: 5 cm</li></ul>',
+            location: {
+				center: [144.97865, -37.82718],
+				zoom: 15.00,
+				pitch: 0.00,
+				bearing: 8.80
+            },
+            mapAnimation: 'jumpTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'denmark',
+            alignment: 'right',
+            hidden: false,
+            title: '12. Denmark',
+            description: '<ul><li><b>Recency</b>: 2019</li><li><b>Resolution</b>: 12.5 cm</li></ul>',
+            location: {
+				center: [12.54340, 55.97050],
+				zoom: 16.27,
+				pitch: 0.00,
+				bearing: 50.40
+            },
+            mapAnimation: 'jumpTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'naip',
+            alignment: 'left',
+            hidden: false,
+            title: '13. NAIP - Continental US',
+            description: '<ul><li><b>Recency</b>: 2018-2019</li><li><b>Resolution</b>: 60-100 cm</li></ul>',
+            location: {
+				center: [-110.02301, 38.45673],
+				zoom: 13.5,
+				pitch: 58,
+				bearing: 19.2
+            },
+            mapAnimation: 'jumpTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'maxar',
+            alignment: 'right',
+            hidden: false,
+            title: '14. Maxar - Global!',
+            description: '<ul><li><b>Recency</b>: 2017-2020</li><li><b>Resolution</b>: 50 cm</li></ul>',
+            location: {
+                center: [131.030318, -25.334351],
+                zoom: 15.31,
+                pitch: 77,
+                bearing: 159.7
+            },
+            mapAnimation: 'jumpTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+
+    ]
+};
